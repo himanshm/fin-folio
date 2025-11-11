dc31743d5bfdcc5ff99e65d412662c72fd5dd4f1
himanshm <sai_himanshu@outlook.com>
2025-11-11 21:14:45 +0530

    feat: integrate Redux and routing into the App component
    Add Redux store and ThemeProvider to the App component for state management and theming. Introduce a new routes file with a basic router setup.

--- apps/fin-folio-client/src/App.tsx
+++ apps/fin-folio-client/src/App.tsx
@@ -1,5 +1,17 @@
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router";
+import { ThemeProvider } from "./contexts";
+import router from "./routes";
+import { store } from "./store/store";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Provider store={store}>
+      <ThemeProvider defaultTheme="system" storageKey="app-theme">
+        <RouterProvider router={router}></RouterProvider>
+      </ThemeProvider>
+    </Provider>
+  );
 };
 
 export default App;
