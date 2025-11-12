f5563e1b3ccc2fcc4885a164a723cac1d1d5e3fe
himanshm <sai_himanshu@outlook.com>
2025-11-12 20:29:59 +0530

    feat: add authentication routes and home route
    Introduce ForgotPassword, Login, Register, and ResetPassword components for user authentication. Update routing configuration to include these new routes, ensuring proper access control with RouteGuard.

--- apps/fin-folio-client/src/routes.tsx
+++ apps/fin-folio-client/src/routes.tsx
@@ -2,6 +2,10 @@ import { createBrowserRouter } from "react-router";
 import RouteGuard from "./components/routes/RouteGuard";
 import DefaultLayout from "./layouts/DefaultLayout";
 import Dashboard from "./pages/Dashboard";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import ResetPassword from "./pages/auth/ResetPassword";
 
 const router = createBrowserRouter([
   {
@@ -9,7 +13,19 @@ const router = createBrowserRouter([
     children: [
       {
         element: <RouteGuard requiredAuth={true} redirectTo="/login" />,
-        children: [{ index: true, Component: Dashboard }]
+        children: [
+          { index: true, Component: Dashboard },
+          { path: "dashboard", Component: Dashboard }
+        ]
+      },
+      {
+        element: <RouteGuard requiredAuth={false} redirectTo="/" />,
+        children: [
+          { path: "login", Component: Login },
+          { path: "register", Component: Register },
+          { path: "forgot-password", Component: ForgotPassword },
+          { path: "reset-password", Component: ResetPassword }
+        ]
       }
     ]
   }
