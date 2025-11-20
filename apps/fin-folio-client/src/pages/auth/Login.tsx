d947704471b15280ff3f1cd355a989cebe0f6dcc
himanshm <sai_himanshu@outlook.com>
2025-11-20 13:24:00 +0530

    feat: implement login functionality
    Enhance the Login component by integrating the AuthForm for user authentication.
    
    Implement handleSubmit to manage login submissions, utilizing the useAuth hook for sign-in logic and navigation upon successful login.

--- apps/fin-folio-client/src/pages/auth/Login.tsx
+++ apps/fin-folio-client/src/pages/auth/Login.tsx
@@ -1,5 +1,22 @@
+import AuthForm from "@/components/AuthForm";
+import { useAuth } from "@/hooks/useAuth";
+import { isLoginDto, type AuthFormData } from "@/types";
+import { useNavigate } from "react-router";
+
 const Login = () => {
-  return <div>Login</div>;
+  const { signIn, isLoading } = useAuth();
+  const navigate = useNavigate();
+  const handleSubmit = async (data: AuthFormData) => {
+    // Since we know this is login mode, we can safely cast
+    // const result = await dispatch(signIn(data as LoginDto));
+    if (isLoginDto(data)) {
+      const result = await signIn(data);
+      if (result.success) {
+        navigate("/", { replace: true });
+      }
+    }
+  };
+  return <AuthForm mode="login" onSubmit={handleSubmit} loading={isLoading} />;
 };
 
 export default Login;
